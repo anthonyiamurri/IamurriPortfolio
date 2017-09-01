8c3a5cb01b42e5df6d404718cbe8a4ef069e9567
Anthony Iamurri <anthony.iamurri@gmail.com>
2017-09-01 02:13:21 -0400

    next commit

---
+++ contact.js
@@ -0,0 +1,10 @@
+function myFunction() {
+    var txt;
+    var person = prompt("Please enter your name:", "First Last");
+    if (person == null || person == "") {
+        txt = "User cancelled the prompt.";
+    } else {
+        txt = "Hello " + person + "! How are you today?";
+    }
+    document.getElementById("demo").innerHTML = txt;
+}
